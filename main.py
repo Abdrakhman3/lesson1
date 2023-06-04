99c22aeed6ef6c924295781912525c437dcb3cd6
Abdrakhman3 <103869807+Abdrakhman3@users.noreply.github.com>
2023-06-04 11:24:31 +0600

    Добавил что-то

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
+Aiperi = "Aiperi"
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
