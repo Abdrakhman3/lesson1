602b7f01fb220373b96d243e4032950ccff5732b
Microchel1 <135493597+Microchel1@users.noreply.github.com>
2023-06-04 11:27:01 +0600

    Add a god

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 Aiperi = "Aiperi"
+Baisal = "Полубог"
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
